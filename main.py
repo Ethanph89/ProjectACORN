2bd8c824c1bdf55c9e6b17bac51fc598a74114a7
Ethanph89 <Ethanph89@gmail.com>
2020-04-03 19:53:31 -0700

    setup base code and todos

---
+++ main.py
@@ -0,0 +1,44 @@
+# IMPORTS---------------------------------------------------------------------------------------------------------------
+import json
+from os import rename, remove
+import os.path
+import PIL.Image
+from PIL import Image, ImageFilter, ImageShow
+import PIL.Image
+import numpy as np
+import boto3
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from tkinter import *
+from pathlib import Path
+import shutil
+import sys
+import random
+import csv
+
+# CLASS DEFINITIONS-----------------------------------------------------------------------------------------------------
+# todo classes:
+#  video
+#  image
+#  loadedCSV
+#  heatmap
+#  timeline
+
+# MAIN FUNCTION---------------------------------------------------------------------------------------------------------
+def main():
+
+
+# BODY FUNCTIONS--------------------------------------------------------------------------------------------------------
+# todo functions:
+#  select video file
+#  parse video to images
+#  send image to API
+#  save data to CSV
+#  select graph
+#  generate graph
+#  display graph
+#  import CSV
+
+# RUN MAIN--------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
