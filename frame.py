f8dd300b18b75b21214d51462f0a258310671072
Deamoriaz <50511284+Deamoriaz@users.noreply.github.com>
2020-05-12 19:34:03 -0700

    Final UI Design. All buttons send appropriate flags. Filepath handling still in development.

--- frame.py
+++ frame.py
@@ -9,6 +9,8 @@ from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
 import os
 import os.path
+import glob
+import ntpath
 import shutil
 import sys
 import random
@@ -44,7 +46,7 @@ banner = Frame(gui,
                bd=3,
                cursor="spider",
                bg="#4b636e",
-               relief="groove")  # TOP justify; contains welcome info / i
+               relief="groove")     # TOP justify; contains welcome info / i
 
 interface = Frame(gui,
                   bd=3,
@@ -56,23 +58,32 @@ display = Frame(gui,
                 bd=3,
                 cursor="dotbox",
                 bg="#a7c0cd",
-                relief="groove")  # RIGHT justify; contains images / graphs
+                relief="groove")    # RIGHT justify; contains images / graphs
 
 options = Frame(interface,
                 bd=1,
                 bg="#a7c0cd",
-                relief="groove")
+                relief="groove")    # Contains radio buttons Video / CSV
+
+shell = Frame(interface,
+              bd=0,
+              bg="#a7c0cd",
+              relief="flat")        # Space to swap between two buttons
 
 checkboxes = Frame(interface,
                    bd=1,
                    bg="#a7c0cd",
                    relief="groove")  # Contains checkboxes hmap / tline
 
+purpose = Frame(interface,
+                bd=0,
+                bg="#a7c0cd",
+                relief="flat")
+
 messages = Frame(interface,
                  bd=1,
                  relief="groove",
-                 bg="#a7c0cd")  # Contains information relevant to user
-
+                 bg="#a7c0cd")      # Contains information relevant to user
 
 # STARTUP DETAIL-------------------------------------------------------------------------------------------------STARTUP
 greeting = "Welcome to Project A.C.O.R.N. \n\n" \
@@ -98,10 +109,31 @@ art = Label(display,
 # canvas.create_image(0, 0, anchor=draw.NW, image=bdSq)
 
 # BUTTONS--------------------------------------------------------------------------------------------------------BUTTONS
-optionVid = Radiobutton()
+a = draw.IntVar()
+a.set(1)
+left = Radiobutton(options,
+                   text="Video",
+                   indicatoron=0,
+                   bg="#a92d2d",
+                   font=cleanFont,
+                   width=10,
+                   padx=20,
+                   variable=a,
+                   value=1,
+                   command=lambda: choice())
 
+right = Radiobutton(options,
+                    text="CSV",
+                    indicatoron=0,
+                    bg="#a92d2d",
+                    font=cleanFont,
+                    width=10,
+                    padx=20,
+                    variable=a,
+                    value=2,
+                    command=lambda: choice())
 
-uploadVideo = Button(interface,
+uploadVideo = Button(shell,
                      image=btn,
                      highlightthickness=0,
                      compound=CENTER,
@@ -113,7 +145,7 @@ uploadVideo = Button(interface,
                      activebackground="#78909c",
                      command=lambda: operation("upload"))
 
-useFile = Button(interface,
+useFile = Button(shell,
                  image=btn,
                  highlightthickness=0,
                  compound=CENTER,
@@ -125,7 +157,7 @@ useFile = Button(interface,
                  activebackground="#78909c",
                  command=lambda: operation("use"))
 
-generate = Button(interface,
+generate = Button(purpose,
                   image=btn,
                   highlightthickness=0,
                   compound=CENTER,
@@ -143,7 +175,7 @@ timechk = IntVar()
 hotmap = Checkbutton(checkboxes,
                      text="Heatmap",
                      font=cleanFont,
-                     bg="#a0552c",
+                     bg="#a92d2d",
                      width=12,
                      variable=heatchk,
                      onvalue=1,
@@ -153,19 +185,31 @@ hotmap = Checkbutton(checkboxes,
 history = Checkbutton(checkboxes,
                       text="Timeline",
                       font=cleanFont,
-                      bg="#a0552c",
+                      bg="#a92d2d",
                       width=12,
                       variable=timechk,
                       onvalue=1,
                       offvalue=0,
                       command=lambda: operation("tline"))
 
+#all = IntVar()
+#marathon = Checkbutton(purpose,
+#                       text="ALL",
+#                       font=cleanFont,
+#                       bg="#a92d2d",
+#                       width=1,
+#                       variable=all,
+#                       onvalue=1,
+#                       offvalue=0,)
+
+
 # INFORMATION DISPLAY----------------------------------------------------------------------------------------INFORMATION
+
 filename = Label(interface,
-                 text="CSV file(s) detected:   ",
+                 text="Loaded: ",
                  wraplength=230,
                  font=cleanFont,
-                 width=26, height=4,
+                 width=26, height=1,
                  relief="groove",
                  bd=1,
                  bg="#a7c0cd")
@@ -185,8 +229,14 @@ text = Label(messages,
 
 # FUNCTIONS----------------------------------------------------------------------------------------------------FUNCTIONS
 
-def test(self):
-    print(self)
+def choice():
+    print(a.get())
+    if a.get() == 1:      # enable Video btn
+        useFile.pack_forget()
+        uploadVideo.pack()
+    else:
+        uploadVideo.pack_forget()
+        useFile.pack()
 
 
 def main():
@@ -210,7 +260,7 @@ def detective():  # Searches for CSV files
 
 def view(bucket):
     for i in range(len(bucket)):
-        filename.config(text=filename.cget("text") + "[" + bucket[i] + "], ")
+        filename.config(text=filename.cget("text") + bucket[i])
 
 
 def selection():
@@ -228,39 +278,45 @@ def selection():
 
 
 def operation(self):
+    csvfile = ""
     print("I received: " + self)  # debugging
     event = ""  # to be packed into description
     if self == 'upload':  # UPLOAD VIDEO OPTION
-        event = "Please select a file for upload."
+        event = "Please select a video file for upload."
         text.configure(text=event)
         package()
         print(event)  # debugging
         handleVideo()
 
     elif self == 'use':  # USE FILE OPTION
-        event = "Please select a .csv file."
+        event = "Please select a CSV file."
         text.configure(text=event)
         package()
 
         print(event)  # debugging
         csvfile = askopenfilename()
         print(csvfile)
-        generateHeatmap(csvfile)
-        artshow()
+        csvfilename = ntpath.basename(csvfile)
+        filename.config(text="Loaded: " + csvfilename)
 
     elif self == 'tline' or self == 'hmap':
         print("A tickbox has been hit")
         selection()
 
     elif self == 'graph':  # GENERATE GRAPHS
+        print(csvfile)
         event = "A graph is being generated \n with the provided data." \
                 "\n\n Please Wait . . ."
         text.configure(text=event)
         package()
         print(event)  # debugging
-        generateTimeline()
-        print("Finished graphing")
-        artshow()  # hang that art on the wall
+
+        if timechk.get() == 1 and csvfile != "":
+            generateTimelineOne(csvfile)
+            print("Finished graphing")
+            artshow()  # hang that art on the wall
+        else:
+            print("Multiple csv files are being handled.")
 
     else:  # what did you break
         print("Uncaught error has occurred.")
@@ -272,20 +328,29 @@ def package():
 
 
 def artshow():
-    for imgfile in os.listdir("saves"):
-        if imgfile.endswith(".png"):
-            print("Found the image! " + imgfile)  # debugging
-            patchwork(imgfile)
-            art.update()
-            text.configure(text="Graph Generated")
-            package()
-        else:
-            print("ERROR: No images exist.")
+    #newest = max([i for i in os.listdir("saves") if i.endswith(".png")], key=os.path.getctime)
+    #print(newest)
+    imgfile = max(glob.iglob('saves/*.png'), key=os.path.getctime)  # get recent PNG file
+    print("Found the image! " + imgfile)  # debugging
+    patchwork(imgfile)
+    art.update()
+    text.configure(text="Graph Generated")
+    package()
+
+    #for imgfile in os.listdir("saves"):
+    #    if imgfile.endswith(".png"):
+    #        print("Found the image! " + imgfile)  # debugging
+    #        patchwork(imgfile)
+    #        art.update()
+    #        text.configure(text="Graph Generated")
+    #        package()
+    #    else:
+    #        print("ERROR: No images exist.")
 
 
 def patchwork(file):
-    print("saves/" + file)
-    graph = Image.open("saves/" + file)
+    print(file)
+    graph = Image.open(file)
     graph = graph.resize((530, 370), Image.ANTIALIAS)
     graph = ImageTk.PhotoImage(graph)  # resized and formatted graphic
     art.configure(image=graph)
@@ -298,30 +363,36 @@ def patchwork(file):
 ###### FRAME: BANNER ######
 banner.pack(side=TOP, fill="x")
 
-headline.pack(fill='x', pady=15)            # headlining text
+headline.pack(fill='x', pady=15)  # headlining text
 
 ###### FRAME: INTERFACE ######
 interface.pack(side=LEFT, fill='x')
 
+options.pack(side=TOP, pady=(20, 10))
+left.pack(side=LEFT)
+right.pack(side=RIGHT)
+shell.pack(side=TOP, pady=(10, 15))
+#uploadVideo.pack(side=TOP, pady=(10, 15))  # upload video file button
+#useFile.pack(side=TOP, pady=(0, 10))  # upload csv file button
+filename.pack(padx=20, pady=10)  # displays csv files
 
-uploadVideo.pack(side=TOP, pady=(10, 15))   # upload video file button
-useFile.pack(side=TOP, pady=(0, 10))                      # upload csv file button
-filename.pack(padx=20, pady=10)             # displays csv files
+checkboxes.pack(side=TOP, pady=(20, 10))  # contains checkboxes
+hotmap.pack(side=LEFT)  # heatmap checkbox
+history.pack(side=LEFT)  # timeline checkbox
 
-checkboxes.pack(side=TOP, pady=(20, 10))    # contains checkboxes
-hotmap.pack(side=LEFT)                      # heatmap checkbox
-history.pack(side=LEFT)                     # timeline checkbox
-generate.pack(pady=(15, 10))                # generate graphs button
+purpose.pack(pady=(15, 20))
+generate.pack(side=LEFT)  # generate graphs button
+#marathon.pack(side=LEFT)
 
-messages.pack(side=BOTTOM, padx=30, pady=(15, 30))  # description/info box
-text.pack()                                 # text inside desc/info box
+messages.pack(side=BOTTOM, padx=30, pady=(30, 30))  # description/info box
+text.pack()  # text inside desc/info box
 
 ###### FRAME: DISPLAY ######
 display.pack(side=RIGHT, fill=BOTH, expand=YES)
 
-art.pack(fill=BOTH, expand=YES)             # graphical display
+art.pack(fill=BOTH, expand=YES)  # graphical display
 
 # SHOWTIME------------------------------------------------------------------------------------------------------SHOWTIME
 filename.config(text=detective())
-
+choice()
 main()
